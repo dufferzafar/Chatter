b30778ac5bc5c5c276476080c73aca0e2ab193cb
Shadab Zafar <dufferzafar0@gmail.com>
2014-11-09 22:05:02 +0530

    Added IRC like chatting support
    Messages can be sent to specific people by starting them with the
    reciever's name.
    
    Use a comma separated list of names to send to multiple people.

--- ChatHandler.java
+++ ChatHandler.java
@@ -2,11 +2,17 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ChatHandler extends Thread
 {
 	protected Socket s;
+
 	protected DataInputStream i;
 	protected DataOutputStream o;
+
+	String username = "";
 	int id;
 
 	public ChatHandler(Socket s, int id) throws IOException
@@ -17,7 +23,7 @@ public class ChatHandler extends Thread
 		o = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
 	}
 
-	protected static Vector handlers=new Vector();
+	protected static Vector handlers = new Vector();
 
 	public void run()
 	{
@@ -26,13 +32,40 @@ public class ChatHandler extends Thread
 			handlers.add(this);  //Add current connection to dynamic array.
 			while(true)
 			{
-				String msg = i.readUTF(); //Read message from client
-				broadcast(msg, this.id);
+				//Read message from client
+				String msg = i.readUTF();
+
+				// Check if a username has been sent
+				Pattern pat = Pattern.compile("Username=(.*)");
+				Matcher mat = pat.matcher(msg);
+				if (mat.find())
+				{
+					this.username = mat.group(1);
+					System.out.println("\tUsername: " + this.username + "\n");
+				}
+				else
+				{
+					// Check if the message is meant for a group of people
+					pat = Pattern.compile("<i>(.*)</i> <b>(.*): </b>(.*): (.*)");
+					mat = pat.matcher(msg);
+					if (mat.find())
+					{
+						msg = "<i>" + mat.group(1) + "</i> <b>" + mat.group(2) + ": </b>" + mat.group(4);
+
+						// Unicast or Multicast
+						sendmessage(msg, this.id, mat.group(3));
+					}
+					else
+					{
+						// Broadcast
+						sendmessage(msg, this.id, "");
+					}
+				}
 			}
 		}
 		catch (IOException ex)
 		{
-			ex.printStackTrace();
+			// ex.printStackTrace();
 		}
 		finally
 		{
@@ -48,11 +81,11 @@ public class ChatHandler extends Thread
 		}
 	}
 
-	public static void broadcast(String message, int id)
+	public static void sendmessage(String msg, int id, String ppl)
 	{
 		synchronized(handlers)
 		{
-			Iterator e=handlers.iterator();  //Iterate through all clients
+			Iterator e=handlers.iterator();
 			while(e.hasNext())
 			{
 				ChatHandler c = (ChatHandler) e.next();
@@ -60,16 +93,31 @@ public class ChatHandler extends Thread
 				{
 					synchronized(c.o)
 					{
-						if(c.id != id) //Check if client is not the sender.
-							c.o.writeUTF(message);
+						// Check if client is not the sender
+						if(c.id != id)
+						{
+							// Broadcast: Send to everyone
+							if (ppl == "")
+							{
+								c.o.writeUTF(msg);
+							}
+							else
+							{
+								// Send only to the correct person
+								if (ppl.contains(c.username))
+								{
+									c.o.writeUTF(msg);
+								}
+							}
 						}
-						c.o.flush();
 					}
-					catch(IOException ex)
-					{
-						c.stop();
-					}
+					c.o.flush();
+				}
+				catch(IOException ex)
+				{
+					c.stop();
 				}
 			}
 		}
 	}
+}
